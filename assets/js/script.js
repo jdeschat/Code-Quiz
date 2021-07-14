4002001b980207c0c23e2cab90f7b88c0425c5b4
Joelle DesChatelets <jdeschat@gmail.com>
2021-07-14 18:20:11 -0400

    added questions and answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,23 +7,23 @@ var quizData = [
         correctAns: 2
     },
     {
-        question: "Commonly used data types DO NOT include",
-        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+        question: "The condition in an if/else statement is enclosed within _____.",
+        answers: ["Quotes", "Curly brackets", "Parentheses", "Square brackets"],
         correctAns: 2
     },
     {
-        question: "Commonly used data types DO NOT include",
-        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+        question: "Arrays in JavaScript can be used to store:",
+        answers: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        correctAns: 3
+    },
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        answers: ["Commas", "Curly brackets", "Quotes", "Parentheses"],
         correctAns: 2
     },
     {
-        question: "Commonly used data types DO NOT include",
-        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
-        correctAns: 2
-    },
-    {
-        question: "Commonly used data types DO NOT include",
-        answers: ["Strings", "Booleans", "Alerts", "Numbers"],
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        answers: ["JavaScript", "Terminal/bash", "For loops", "console.log"],
         correctAns: 2
     }
 ];
